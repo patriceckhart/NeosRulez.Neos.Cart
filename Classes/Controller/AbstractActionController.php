6e2e95a607116ddeb3e1207b1a9744a0b3e9f3ab
Patric Eckhart <mail@patriceckhart.com>
2023-05-24 15:36:44 +0200

    allow all properties

--- Classes/Controller/AbstractActionController.php
+++ Classes/Controller/AbstractActionController.php
@@ -8,6 +8,7 @@ namespace NeosRulez\Neos\Cart\Controller;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Controller\ActionController;
 use Neos\Flow\Mvc\View\JsonView;
+use Neos\Flow\Property\TypeConverter\PersistentObjectConverter;
 use Neos\Fusion\View\FusionView;
 
 /**
@@ -25,5 +26,18 @@ abstract class AbstractActionController extends ActionController
         'html' => FusionView::class,
         'json' => JsonView::class,
     ];
+
+    protected function initializeAction()
+    {
+        $arguments = $this->request->getArguments();
+        foreach ($arguments as $argumentIterator => $argument) {
+            $propertyMappingConfiguration = $this->arguments[$argumentIterator]->getPropertyMappingConfiguration();
+            $propertyMappingConfiguration->setTypeConverterOption(PersistentObjectConverter::class,
+                PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true );
+            $propertyMappingConfiguration->setTypeConverterOption(PersistentObjectConverter::class,
+                PersistentObjectConverter::CONFIGURATION_MODIFICATION_ALLOWED, true );
+            $propertyMappingConfiguration->allowAllProperties()->skipUnknownProperties();
+        }
+    }
 
 }
